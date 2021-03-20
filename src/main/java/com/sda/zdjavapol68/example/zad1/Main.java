c92bb37c4786877a8a726aec12ff629f53d3ffa9
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-20 09:29:33 +0100

    -- zad1 scratch

---
+++ src/main/java/com/sda/zdjavapol68/example/zad1/Main.java
@@ -0,0 +1,19 @@
+package com.sda.zdjavapol68.example.zad1;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> unsorted = List.of("A", "B", "b", "Z", "a", "c");
+
+        List<String> sorted = sort(unsorted);
+
+        System.out.println(sorted);
+    }
+
+    private static List<String> sort(List<String> unsorted) {
+        return unsorted;
+    }
+}

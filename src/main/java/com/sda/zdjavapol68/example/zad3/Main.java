2959f13d1399acae7e8368028c1d501ce810366e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-20 10:20:46 +0100

    -- zad3 immutable map created

--- src/main/java/com/sda/zdjavapol68/example/zad3/Main.java
+++ src/main/java/com/sda/zdjavapol68/example/zad3/Main.java
@@ -6,7 +6,21 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
+        //old school - mutable map
+        /*Map<String, Integer> map = new HashMap<>();
+        map.put("Java", 18);
+        map.put("C#", 9);
+        map.put("Python", 1);
+        map.put("JavaScript", 0);
+        map.put("PHP", 0);*/
+
+        //java 11 style - immutable map
+        Map<String, Integer> map = Map.of("Java", 18,
+                "C#", 9,
+                "Python", 1,
+                "JavaScript", 0,
+                "PHP", 0);
+
         print(map);
     }
 

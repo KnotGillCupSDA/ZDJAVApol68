b770191d46938b5c293cbc2cea65ac5befa61bcc
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-20 10:58:21 +0100

    -- zad 4 scratch

---
+++ src/main/java/com/sda/zdjavapol68/example/zad4/Storage.java
@@ -0,0 +1,25 @@
+package com.sda.zdjavapol68.example.zad4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+
+    private final Map<String, List<String>> storage;
+
+    public Storage() {
+        storage = new HashMap<>();
+    }
+
+    public void addToStorage(String key, String value){
+        //storage.put(key, value);
+    }
+
+    public void printValues(String key) {
+    }
+
+    public void findValues(String value) {
+
+    }
+}

905455277864d2e5e68a649689d8b5e8aa2c1429
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-20 11:00:57 +0100

    -- zad 4 scratch Main

---
+++ src/main/java/com/sda/zdjavapol68/example/zad4/Main.java
@@ -0,0 +1,15 @@
+package com.sda.zdjavapol68.example.zad4;
+
+public class Main {
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+
+        storage.addToStorage("klucz1", "wartosc1");
+        storage.addToStorage("klucz1", "wartosc2");
+
+        storage.printValues("klucz1");
+
+        storage.findValues("wartosc1");
+
+    }
+}

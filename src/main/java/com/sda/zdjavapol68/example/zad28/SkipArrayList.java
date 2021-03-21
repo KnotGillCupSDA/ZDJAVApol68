5c4696f56aa037480ab8b5b0435b518fdd10401e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-21 11:52:18 +0100

    -- zad 29 method signature

--- src/main/java/com/sda/zdjavapol68/example/zad28/SkipArrayList.java
+++ src/main/java/com/sda/zdjavapol68/example/zad28/SkipArrayList.java
@@ -1,6 +1,11 @@
 package com.sda.zdjavapol68.example.zad28;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SkipArrayList<E> extends ArrayList<E> {
+
+    public List<E> getEveryNthElement(int startIndex, int skip) {
+
+    }
 }

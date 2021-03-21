57e3d22b01c770c546a599f6d8895d9fb12986ca
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-21 13:36:08 +0100

    -- zad 30 read file

--- src/main/java/com/sda/zdjavapol68/example/zad30/Main.java
+++ src/main/java/com/sda/zdjavapol68/example/zad30/Main.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Path resourceDir = Paths.get("src", "main", "resources");
-
         Path inputFile = resourceDir.resolve("sometextfile.txt");
 
-        System.out.println(Files.readString(inputFile));
+        String originalText = Files.readString(inputFile);
+        System.out.println(originalText);
+
+        Files.writeString(resourceDir.resolve("test.txt"),
+                originalText,
+                StandardOpenOption.CREATE);
 
     }
 }

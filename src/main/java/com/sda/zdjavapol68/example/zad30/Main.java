900bd5b91820d60c2e9de2b949b0d8c055ac4b5e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-21 13:22:40 +0100

    -- zad 30 scratch

---
+++ src/main/java/com/sda/zdjavapol68/example/zad30/Main.java
@@ -0,0 +1,11 @@
+package com.sda.zdjavapol68.example.zad30;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path resourceDir = Paths.get("src", "main", "resources");
+    }
+}

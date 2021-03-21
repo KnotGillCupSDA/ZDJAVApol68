e94594aa97eea455648d1453da63021ea9fd3426
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-21 12:26:32 +0100

    -- zad 28 done

--- src/main/java/com/sda/zdjavapol68/example/zad28/SkipArrayList.java
+++ src/main/java/com/sda/zdjavapol68/example/zad28/SkipArrayList.java
@@ -1,12 +1,33 @@
 package com.sda.zdjavapol68.example.zad28;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SkipArrayList<E> extends ArrayList<E> {
 
     public List<E> getEveryNthElement(int startIndex, int skip) {
+        if (startIndex < 0 | startIndex >= size()) {
+            return Collections.emptyList();
+        }
         List<E> skipped = new ArrayList<>();
+
+        /*int currentIndex = startIndex;
+        while (currentIndex < size()) {
+            skipped.add(get(currentIndex));
+            currentIndex += skip + 1;
+        }*/
+
+        for (int i = startIndex; i < size(); i += skip + 1) {
+            skipped.add(get(i));
+        }
+
         return skipped;
+    }
+
+    @Override
+    public int size() {
+        System.out.println("my size");
+        return super.size();
     }
 }
--- src/test/java/com/sda/zdjavapol68/example/zad28/SkipArrayListTest.java
+++ src/test/java/com/sda/zdjavapol68/example/zad28/SkipArrayListTest.java
@@ -1,19 +1,50 @@
 package com.sda.zdjavapol68.example.zad28;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class SkipArrayListTest {
 
+    private SkipArrayList<String> skipArrayList;
+
+    @BeforeEach
+    void setUp() {
+        skipArrayList = new SkipArrayList<>();
+    }
+
     @Test
     void thatWeCanGetEveryNthElement() {
-        SkipArrayList<String> strings = new SkipArrayList<>();
-        strings.addAll(List.of("a", "b", "c", "d", "e", "f", "g"));
+        skipArrayList.addAll(List.of("a", "b", "c", "d", "e", "f", "g"));
+        List<String> everyNthElement = skipArrayList.getEveryNthElement(2, 3);
 
-        List<String> everyNthElement = strings.getEveryNthElement(2, 3);
+        Assertions.assertTrue(everyNthElement.containsAll(List.of("c", "g")), "invalid list: " + everyNthElement);
+    }
 
-        Assertions.assertTrue(everyNthElement.containsAll(List.of("c", "g")));
+    @Test
+    void thatWeCanGetEveryNthIntegerElement() {
+        SkipArrayList<Integer> skipArrayList = new SkipArrayList<>();
+        skipArrayList.addAll(List.of(0, 1, 2, 3, 4, 5, 6));
+        List<Integer> everyNthElement = skipArrayList.getEveryNthElement(1, 2);
+
+        Assertions.assertTrue(everyNthElement.containsAll(List.of(1, 4)), "invalid list: " + everyNthElement);
+    }
+
+    @Test
+    void testTooHighStartingIndex() {
+        skipArrayList.addAll(List.of("a", "b", "c", "d", "e", "f", "g"));
+        List<String> everyNthElement = skipArrayList.getEveryNthElement(12213, 3);
+
+        Assertions.assertEquals(0, everyNthElement.size());
+    }
+
+    @Test
+    void testStartingIndexIsBelowZero() {
+        skipArrayList.addAll(List.of("a", "b", "c", "d", "e", "f", "g"));
+        List<String> everyNthElement = skipArrayList.getEveryNthElement(-12213, 3);
+
+        Assertions.assertEquals(0, everyNthElement.size());
     }
 }

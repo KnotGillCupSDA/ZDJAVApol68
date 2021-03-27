89c8bfc354ce1ab056aa1c79625c83609b454419
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-03-27 09:34:23 +0100

    -- zad 32 draft

---
+++ src/main/java/com/sda/zdjavapol68/example/zad32/Car.java
@@ -0,0 +1,38 @@
+package com.sda.zdjavapol68.example.zad32;
+
+public class Car {
+
+    private String name;
+    private String model;
+    private double price;
+
+    public Car(String name, String model, double price) {
+        this.name = name;
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/sda/zdjavapol68/example/zad32/CarStorage.java
@@ -0,0 +1,19 @@
+package com.sda.zdjavapol68.example.zad32;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class CarStorage {
+
+    public CarStorage(Path path) {
+
+    }
+
+    public void save(List<Car> cars) {
+
+    }
+
+    public List<Car> load() {
+        return null;
+    }
+}
